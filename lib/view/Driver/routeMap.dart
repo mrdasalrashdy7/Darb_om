0be872937a43e3b18b3256d47d15e0ce6d7553e3
mrdasalrashdy7 <mrdasalrashdy7@gmail.com>
2024-12-12 21:41:46 +0400

    add map page for driver

---
+++ lib/view/Driver/routeMap.dart
@@ -0,0 +1,75 @@
+import 'package:darb/controller/map_controller.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class RouteMap extends StatelessWidget {
+  final MyMapController mapController = Get.put(MyMapController());
+  final List
+      Points; //take these point into markers with points info (latlong, name, phone, home image, custom instructions-optional)
+
+  RouteMap({super.key, required this.Points});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: TextField(
+          decoration: InputDecoration(
+            hintText: 'Search location...',
+            border: InputBorder.none,
+            contentPadding: EdgeInsets.all(10.0),
+          ),
+          onSubmitted: (value) {
+            // You can add functionality here to handle search actions
+          },
+        ),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.clear),
+            onPressed: () {
+              mapController.clearMap();
+            },
+          ),
+        ],
+      ),
+      body: Obx(() {
+        if (mapController.initialPosition.value.latitude == 0.0 &&
+            mapController.initialPosition.value.longitude == 0.0) {
+          return Center(child: CircularProgressIndicator());
+        }
+        return GoogleMap(
+          mapType: MapType.normal,
+          initialCameraPosition: CameraPosition(
+            target: mapController.initialPosition.value,
+            zoom: 11.0,
+          ),
+          markers: Set<Marker>.of(mapController.markers),
+          polylines: Set<Polyline>.of(mapController.polylines),
+          circles: Set<Circle>.of(
+              [mapController.userLocationCircle.value]), // Add the circle here
+          onMapCreated: (GoogleMapController controller) {
+            mapController.mapController = controller;
+          },
+          onTap: (point) {
+            mapController.addCustomPoint(point);
+          },
+          // Set gesture handling for Flutter Web
+        );
+      }),
+      floatingActionButton: FloatingActionButton.extended(
+        backgroundColor: Colors.orange,
+        onPressed: () {
+          mapController.showMainBottomSheet(context);
+        },
+        label: Row(
+          children: const [
+            Icon(Icons.route),
+            SizedBox(width: 5),
+            Text("show options"),
+          ],
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+    );
+  }
+}
